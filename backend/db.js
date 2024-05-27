40d806db752ee1ed14934a4771ce8467255c2773
dhruvm911 <dhruvmaheshwari9411@gmail.com>
2024-05-27 15:21:39 +0530

    login sgnup finalized

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const {MongoClient} = require("mongodb");
 
 module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
+    // const connectionParams = {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // };
     try {
-        mongoose.connect(process.env.DB,connectionParams);
+        mongoose.connect(process.env.DB,{});
         console.log("Connected to database successfully")
     } catch (error) {
         console.log(error);
