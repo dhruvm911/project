2c991f2eaa19dfec698b324f18e5d20a6c97e41b
dhruvm911 <dhruvmaheshwari9411@gmail.com>
2024-06-08 17:34:59 +0530

    Backend part of login-signup along with user routes and controllers along with post routes and controllers done

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 require("dotenv").config();
+const upload = require('express-fileupload');
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
-const userRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 connection();
 
-app.use(express.json());
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(upload());
+app.use('/uploads',express.static(__dirname + '/uploads'))
 
 app.use("/api/users",userRoutes);
-app.use("/api/auth",authRoutes);
+app.use("/api/posts",postRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
 
 const port = process.env.PORT||8080;
-app.listen(port,() => console.log('Listening on port ${port}...'));
+app.listen(port,() => console.log(`Listening on port ${port}...`));
